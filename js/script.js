eaa0310818c8ccf9e58edcfcae01ebb76c4288d9
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-04 15:25:40 +0100

    inserito codice js e html per visualizzazione informazioni in pagina

--- js/script.js
+++ js/script.js
@@ -6,20 +6,24 @@ const age = prompt('Inserisci la tua età');
 
 let priceForLenght = travelLenght * 0.21;
 
-console.log(priceForLenght);
-
 if (age < 18) {
     discount = (priceForLenght * 20 / 100)
 
     finalPrice = priceForLenght - discount
 
-    console.log(finalPrice)
 } else if (age >= 65) {
     discount = (priceForLenght * 40 / 100)
 
     finalPrice = priceForLenght - discount
 
-    console.log(finalPrice)
 } else {
     finalPrice = priceForLenght
 }
+
+console.log(finalPrice)
+
+document.getElementById('Km').innerText = 'Km da percorrere: ' + travelLenght;
+
+document.getElementById('eta').innerText = 'Anni: ' + age;
+
+document.getElementById('ticket').innerText = 'Prezzo finale: ' + finalPrice;
