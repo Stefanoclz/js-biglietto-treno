3233225c68a85c67108c4c133ba8f677ff70ebb0
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-04 14:53:54 +0100

    aggiunta variabile per calcolo prezzo a km

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 console.log('JS Ok')
 
-const travelLenght = prompt('Inserisci la lunghezza del viaggio in KM')
+const travelLenght = prompt('Inserisci la lunghezza del viaggio in KM');
 
-const age = prompt('Inserisci la tua età')
+const age = prompt('Inserisci la tua età');
+
+let priceForLenght = travelLenght * 0.21;
+
+console.log(priceForLenght);
