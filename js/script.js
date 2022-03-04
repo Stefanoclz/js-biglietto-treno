f45159c6f3409f90ad5c4a275afb5c8c7c7244fa
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-04 15:18:50 +0100

    aggiunti calcoli per prezzo biglietto e sconto

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ const age = prompt('Inserisci la tua età');
 let priceForLenght = travelLenght * 0.21;
 
 console.log(priceForLenght);
+
+if (age < 18) {
+    discount = (priceForLenght * 20 / 100)
+
+    finalPrice = priceForLenght - discount
+
+    console.log(finalPrice)
+} else if (age >= 65) {
+    discount = (priceForLenght * 40 / 100)
+
+    finalPrice = priceForLenght - discount
+
+    console.log(finalPrice)
+} else {
+    finalPrice = priceForLenght
+}
